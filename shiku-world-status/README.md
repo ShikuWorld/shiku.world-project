3b3a5f6ac42d55c2b3b5676290e482c3509c2a20
shiku <u0iqf4xm@anonaddy.me>
2023-10-08 19:24:35 +0200

    make status service actually do what it is supposed to

--- shiku-world-status/README.md
+++ shiku-world-status/README.md
@@ -1,2 +1,7 @@
 docker build -t status-service .
 docker run -p 3000:3000 --name status-service-container -d status-service
+
+# env variables
+
+GET_MAIN_DOOR_STATUS_URL=http://127.0.0.1:3030/main-door-status
+GET_BACK_DOOR_STATUS_URL=http://127.0.0.1:3030/back-door-status
--- shiku-world-status/src/index.ts
+++ shiku-world-status/src/index.ts
@@ -1,6 +1,10 @@
 import Fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
 import path from 'path';
+import axios from 'axios';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 const fastify = Fastify({
   logger: true
@@ -12,6 +16,45 @@ fastify.register(fastifyStatic, {
 });
 fastify.get('/', async (_request, reply) => {
   return reply.sendFile('./index.html', __dirname);
+});
+
+type DoorStatusCheck =
+  | { type: 'open' }
+  | { type: 'lightsOn' }
+  | { type: 'lightsOut' }
+  | { type: 'urlNotConfigured' }
+  | { type: 'unknownError'; error: Error };
+
+async function getDoorStatus(
+  statusUrl: string | undefined
+): Promise<DoorStatusCheck> {
+  try {
+    if (!statusUrl) {
+      return { type: 'urlNotConfigured' };
+    }
+    return (await axios.get<boolean>(statusUrl)).data
+      ? { type: 'open' }
+      : { type: 'lightsOn' };
+  } catch (e) {
+    if (e instanceof Error) {
+      if (e.message.includes('ECONNREFUSED')) {
+        return { type: 'lightsOut' };
+      }
+      return { type: 'unknownError', error: e };
+    }
+
+    return {
+      type: 'unknownError',
+      error: new Error(`Unknown error occurred ${e}`)
+    };
+  }
+}
+fastify.get('/main-door-status', async (request, reply) => {
+  return reply.send(await getDoorStatus(process.env.GET_MAIN_DOOR_STATUS_URL));
+});
+
+fastify.get('/back-door-status', async (request, reply) => {
+  return reply.send(await getDoorStatus(process.env.GET_BACK_DOOR_STATUS_URL));
 });
 
 fastify.listen({ host: '0.0.0.0', port: 3000 }, (err, _address) => {
